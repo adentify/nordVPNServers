8dad8712e2ef5121b3c7f1bd2c8d348669c014a3
Sean Dillon <sean@adentify.net>
2022-01-18 18:16:52 +0000

    removing dupliace heder()

--- tools/test_proxy_http.php
+++ tools/test_proxy_http.php
@@ -1,5 +1,4 @@
 <?php
-  header('Content-Type:text/plain');
 
   $conf["url"]           = "https://ipapi.co/json/";
   $conf["useragent"]     = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1";
